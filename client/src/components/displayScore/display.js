9cd87fe2c84bd8bf42ba652eb633088576a26301
Prateek Sharma <prateekshrmprateek@gmail.com>
2021-04-27 18:29:18 +0530

    displaying the scoreCard

--- client/src/components/displayScore/display.js
+++ client/src/components/displayScore/display.js
@@ -8,6 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import _ from 'lodash';
+
 const useStyles = makeStyles({
     table: {
         minWidth: 500,
@@ -23,13 +24,22 @@ const Display = (props) => {
             <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Games</TableCell>
+                        <TableCell>Tic Tac Toe Games</TableCell>
                         <TableCell align="right">You </TableCell>
                         <TableCell align="right">Computer</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
+                    {!_.isEmpty(props.score) ? props.score.map((row, idx) => (
+                        <TableRow key={idx}>
+                            <TableCell component="th" scope="row">
+                                Game {idx + 1}
+                            </TableCell>
+                            <TableCell align="right">{row === "You won" ? 1 : row === "You lost" ? 0 : 0}</TableCell>
+                            <TableCell align="right">{row === "You won" ? 0 : row === "You lost" ? 1 : 0}</TableCell>
 
+                        </TableRow>
+                    )) : <TableRow><TableCell>No Games Played Yet</TableCell></TableRow>}
                 </TableBody>
             </Table>
         </TableContainer>
